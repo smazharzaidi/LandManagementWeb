3e591d3875735eb5542d4c1f032f72fe3ce0fe86
Nehal567 <133264581+Nehal567@users.noreply.github.com>
2023-05-14 13:47:18 +0500

    abc

--- resources/views/seller/read.blade.php
+++ resources/views/seller/read.blade.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <h2 style="border:1px solid black;background-color:DodgerBlue; text-align:center;">Add User</h2>
+    @if (session('status'))
+    <div class="alert alert-success alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {{ session('status') }}
+    </div>
+
+    @endif
+
 <table>
     <thead>
         <tr>
@@ -35,4 +58,8 @@
 
         </td>
     </tr>
+    @endforeach
 </tbody>
+</table>
+</body>
+</html>
