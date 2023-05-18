802f59f183981018baa8f83b9f52292b0c910a12
smazharzaidi <35042724+smazharzaidi@users.noreply.github.com>
2023-05-18 19:07:45 +0500

    Initial commit

--- resources/views/land/create.blade.php
+++ resources/views/land/create.blade.php
@@ -3,23 +3,84 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Land</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
 
+        h2 {
+            text-align: center;
+            padding: 10px;
+            background-color: #388E3C;
+            color: white;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 500px;
+            margin-bottom: 30px;
+        }
+
+        label {
+            margin-top: 10px;
+        }
+
+        input[type="text"] {
+            margin-top: 5px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 100%;
+        }
+
+        input[type="submit"] {
+            margin-top: 10px;
+            padding: 10px 20px;
+            border: none;
+            background-color: #388E3C;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #2E7D32;
+        }
+
+        .img-container {
+            width: 100%;
+            height: 200px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: url('https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?cs=srgb&dl=pexels-%C3%A1kos-szab%C3%B3-440731.jpg&fm=jpg') no-repeat center center;
+            background-size: cover;
+            background-position: center 75%;
+        }
+    </style>
 </head>
 
 <body>
-    <h2 style="border: 1px solid black; background-color:DodgerBlue; text-align:center;">
-        Add New Land
-    </h2>
-    <!-- For Redirecting With Flashed Session Data when 'Submit' button -->
-    <!-- is pressed in the 'create.blade.php' view which calls the relevant -->
-    <!-- function 'store' in the StudentController and then this -->
-    <!-- view, 'create.blade.php' is again called -->
+    <div class="img-container">
+    </div>
+    <h2>Add New Land</h2>
     <!-- START -->
     @if (session('status'))
     <div class="alert alert-success alert-dismissible">
@@ -31,13 +92,13 @@
     <form action="{{ route ('land.store') }}" method="post">
         @csrf
         <label for="tehsil">Tehsil: &nbsp;</label>
-        <input type="text" id="tehsil" name="tehsil" value="Bhutesar"><br><br>
+        <input type="text" id="tehsil" name="tehsil" value=""><br>
         <label for="khasra_number">Khasra Number: &nbsp;</label>
-        <input type="text" id="khasra_number" name="khasra_number" value="1"><br><br>
+        <input type="text" id="khasra_number" name="khasra_number" value=""><br>
         <label for="division">Division: (if any)&nbsp;</label>
-        <input type="text" id="division" name="division" value="(a)"><br><br>
+        <input type="text" id="division" name="division" value=""><br>
         <label for="patwari">Patwari Name: &nbsp;</label>
-        <input type="text" id="patwari" name="patwari" value="Kashif Abbas"><br><br>
+        <input type="text" id="patwari" name="patwari" value=""><br>
         <input type="submit" value="Submit">
     </form>
 </body>
--- resources/views/land/index.blade.php
+++ resources/views/land/index.blade.php
@@ -1,30 +1,139 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lands List</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        h1 {
+            text-align: center;
+            padding: 10px;
+            background-color: #388E3C;
+            color: white;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
         table {
             border-collapse: collapse;
             width: 100%;
+            max-width: 1000px;
+            margin: 0 auto;
+            table-layout: fixed;
         }
 
         th,
         td {
             border: 1px solid black;
             padding: 8px;
+            text-align: center;
+            vertical-align: middle;
+            word-wrap: break-word;
         }
 
         th {
-            background-color: #f2f2f2;
+            background-color: #388E3C;
+            color: white;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            max-width: 1000px;
+            margin-bottom: 30px;
+        }
+
+        .container form {
+            display: flex;
+            justify-content: center;
+            gap: 10px;
+            margin-top: 10px;
+            width: 100%;
+            max-width: 800px;
+        }
+
+        .container form button {
+            padding: 10px 20px;
+            border: none;
+            background-color: #388E3C;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .container form button:hover {
+            background-color: #2E7D32;
+        }
+
+        .container form .action-buttons {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+            height: 100%;
+        }
+
+        .container form .action-buttons button {
+            padding: 10px 20px;
+            border: none;
+            background-color: #388E3C;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .container form .action-buttons button:hover {
+            background-color: #2E7D32;
+        }
+
+        .img-container {
+            width: 100%;
+            height: 200px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: url('https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?cs=srgb&dl=pexels-%C3%A1kos-szab%C3%B3-440731.jpg&fm=jpg') no-repeat center center;
+            background-size: cover;
+            background-position: center 75%;
+        }
+
+        .action-buttons form {
+            display: inline-block;
+            float: left;
+            margin-right: 10px;
+        }
+
+        .action-buttons {
+            display: inline-flex;
+            justify-content: space-between;
         }
     </style>
 </head>
 
 <body>
+    <div class="img-container"></div>
+    <h1>Lands List</h1>
     <div class="container">
-        <h1>Lands List</h1>
         <table>
             <thead>
                 <tr>
@@ -43,22 +152,22 @@
                     <td>{{ $land->division }}</td>
                     <td>{{ $land->patwari }}</td>
                     <td>
-                        <form action="{{ route('land.destroy', $land->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                        <form action="{{ route('land.edit', $land->id) }}" method="GET">
-                            @csrf
-                            <button type="submit" class="btn btn-primary">Edit</button>
-                        </form>
+                        <div class="action-buttons">
+                            <form action="{{ route('land.confirmDelete', $land->id) }}" method="GET" class="delete-form">
+                                @csrf
+                                <button type="submit">Delete</button>
+                            </form>
+                            <form action="{{ route('land.edit', $land->id) }}" method="GET" class="edit-form">
+                                <button type="submit">Edit</button>
+                            </form>
+                        </div>
                     </td>
+
                 </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-
 </body>
 
 </html>
