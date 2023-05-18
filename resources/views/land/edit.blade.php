3a391f3ae16c176c3b0a8724e903903d9e67faa2
smazharzaidi <35042724+smazharzaidi@users.noreply.github.com>
2023-05-18 20:45:15 +0500

    Bhai ho ja upload

--- resources/views/land/edit.blade.php
+++ resources/views/land/edit.blade.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Land</title>
+    <title>Edit Land Record</title>
     <style>
         body {
             margin: 0;
@@ -17,7 +15,16 @@
             background-color: #f4f4f4;
         }
 
-        h2 {
+        .container {
+            width: 500px;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+        }
+
+        h1 {
             text-align: center;
             padding: 10px;
             background-color: #388E3C;
@@ -26,42 +33,39 @@
             border-radius: 5px;
         }
 
+        .alert {
+            background-color: #f8d7da;
+            color: #721c24;
+            border-color: #f5c6cb;
+            padding: 10px;
+            border-radius: 5px;
+        }
+
         form {
             display: flex;
+            justify-content: center;
+            margin-top: 20px;
             flex-direction: column;
-            align-items: center;
-            padding: 20px;
-            background-color: white;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 500px;
-            margin-bottom: 30px;
         }
 
-        label {
-            margin-top: 10px;
+        .form-group {
+            margin-bottom: 20px;
         }
 
-        input[type="text"] {
-            margin-top: 5px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            width: 100%;
-        }
-
-        input[type="submit"] {
-            margin-top: 10px;
-            padding: 10px 20px;
-            border: none;
-            background-color: #388E3C;
+        button.btn-primary {
+            background-color: #007bff;
             color: white;
+            border: none;
+            padding: 10px 20px;
             border-radius: 5px;
             cursor: pointer;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            text-decoration: none;
         }
 
-        input[type="submit"]:hover {
-            background-color: #2E7D32;
+        .btn-primary:hover {
+            background-color: #0069d9;
         }
 
         .img-container {
@@ -74,33 +78,59 @@
             background-size: cover;
             background-position: center 75%;
         }
+
+        .btn-secondary {
+            background-color: #6c757d;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            text-decoration: none;
+        }
+
+        .btn-secondary:hover {
+            background-color: #5a6268;
+        }
     </style>
 </head>
 
 <body>
-    <div class="img-container">
+    <div class="img-container"></div>
+    <div class="container">
+        <h1>Edit Land Record</h1>
+        @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form method="POST" action="{{ route('land.update', $land->id) }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="tehsil">Tehsil:</label>
+                <input type="text" class="form-control" id="tehsil" name="tehsil" value="{{ $land->tehsil }}" required>
+            </div>
+            <div class="form-group">
+                <label for="khasra_number">Khasra Number:</label>
+                <input type="text" class="form-control" id="khasra_number" name="khasra_number" value="{{ $land->khasra_number }}" required>
+            </div>
+            <div class="form-group">
+                <label for="division">Division:</label>
+                <input type="text" class="form-control" id="division" name="division" value="{{ $land->division }}" required>
+            </div>
+            <div class="form-group">
+                <label for="patwari">Patwari:</label>
+                <input type="text" class="form-control" id="patwari" name="patwari" value="{{ $land->patwari }}" required>
+            </div>
+            <div class="btn-container">
+                <button type="submit" class="btn btn-primary">Update</button>
+                <a href="{{ route('land.index') }}" class="btn btn-secondary">Cancel</a>
+            </div>
+        </form>
     </div>
-    <h2>Add New Land</h2>
-    <!-- START -->
-    @if (session('status'))
-    <div class="alert alert-success alert-dismissible">
-        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        {{ session('status') }}
-    </div>
-    @endif
-    <!-- END -->
-    <form action="{{ route ('land.store') }}" method="post">
-        @csrf
-        <label for="tehsil">Tehsil:</label>
-        <input type="text" id="tehsil" name="tehsil" value=""><br>
-        <label for="khasra_number">Khasra Number:</label>
-        <input type="text" id="khasra_number" name="khasra_number" value=""><br>
-        <label for="division">Division (if any):</label>
-        <input type="text" id="division" name="division" value=""><br>
-        <label for="patwari">Patwari Name:</label>
-        <input type="text" id="patwari" name="patwari" value=""><br>
-        <input type="submit" value="Submit">
-    </form>
 </body>
 
 </html>
