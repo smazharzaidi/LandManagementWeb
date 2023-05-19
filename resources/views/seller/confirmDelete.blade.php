9a5b219272ce9fa8532e59b5f40353cc32c00638
smazharzaidi <35042724+smazharzaidi@users.noreply.github.com>
2023-05-19 09:26:14 +0500

    Mujhy ghar jaana hy

--- resources/views/seller/confirmDelete.blade.php
+++ resources/views/seller/confirmDelete.blade.php
@@ -5,24 +5,118 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirm Deletion</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: grid;
+            place-items: center;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            width: 500px;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+        }
+
+        h2 {
+            text-align: center;
+            padding: 10px;
+            background-color: #388E3C;
+            color: white;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        .alert {
+            background-color: #f8d7da;
+            color: #721c24;
+            border-color: #f5c6cb;
+            padding: 10px;
+            border-radius: 5px;
+        }
+
+        form {
+            display: grid;
+            place-items: center;
+            margin-top: 20px;
+        }
+
+        .btn-container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 10px;
+            place-items: center;
+            margin-top: 10px;
+        }
+
+        .img-container {
+            width: 100%;
+            height: 200px;
+            display: grid;
+            place-items: center;
+            background: url('https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?cs=srgb&dl=pexels-%C3%A1kos-szab%C3%B3-440731.jpg&fm=jpg') no-repeat center center;
+            background-size: cover;
+            background-position: center 75%;
+        }
+
+        .btn-danger {
+            background-color: #dc3545;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .btn-secondary {
+            background-color: #6c757d;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            text-decoration: none;
+        }
+
+        .btn-secondary:hover {
+            background-color: #5a6268;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Are you sure you want to delete this seller?</h1>
+    <div class="img-container"></div>
+    <div class="container">
+        <h2>Delete Seller</h2>
+        <div class="alert">
+            <p>Are you sure you want to delete this seller?</p>
 
-    <h2>Seller Details</h2>
-    <p><strong>CNIC:</strong> {{ $seller->cnic }}</p>
-    <p><strong>Name:</strong> {{ $seller->name }}</p>
-    <p><strong>Email:</strong> {{ $seller->email }}</p>
-    <p><strong>Phone Number:</strong> {{ $seller->phone_number }}</p>
-    <p><strong>Address:</strong> {{ $seller->address }}</p>
-    <p><strong>User Type:</strong> {{ $seller->user_type }}</p>
+            <p><strong>CNIC:</strong> {{ $seller->cnic }}</p>
+            <p><strong>Name:</strong> {{ $seller->name }}</p>
+            <p><strong>Email:</strong> {{ $seller->email }}</p>
+            <p><strong>Phone Number:</strong> {{ $seller->phone_number }}</p>
+            <p><strong>Address:</strong> {{ $seller->address }}</p>
+            <p><strong>User Type:</strong> {{ $seller->user_type }}</p>
 
-    <form action="{{ route('seller.destroy', $seller->id) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
+            <form action="{{ route('seller.destroy', $seller->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <div class="btn-container">
+                    <input type="submit" class="btn btn-danger" value="Yes, Delete it!">
+                    <a href="{{ route('seller.read') }}" class="btn btn-secondary">No, Go Back!</a>
+
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
--- resources/views/seller/update.blade.php
+++ resources/views/seller/update.blade.php
@@ -3,56 +3,130 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create User</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Update Seller Record</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: grid;
+            place-items: center;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        h2 {
+            text-align: center;
+            padding: 10px;
+            background-color: #388E3C;
+            color: white;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        .container {
+            display: grid;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 500px;
+            margin-bottom: 30px;
+        }
+
+        .form-group {
+            margin-top: 10px;
+        }
+
+        input[type="text"],
+        select {
+            margin-top: 5px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 100%;
+        }
+
+        input[type="submit"] {
+            margin-top: 10px;
+            padding: 10px 20px;
+            border: none;
+            background-color: #388E3C;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #2E7D32;
+        }
+
+        .img-container {
+            width: 100%;
+            height: 200px;
+            display: grid;
+            place-items: center;
+            background: url('https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?cs=srgb&dl=pexels-%C3%A1kos-szab%C3%B3-440731.jpg&fm=jpg') no-repeat center center;
+            background-size: cover;
+            background-position: center 75%;
+        }
+    </style>
 </head>
 
-<html>
-
 <body>
-    <h2 style="border:1px solid black;background-color:DodgerBlue; text-align:center;">
-        Update Seller Record
-    </h2>
+    <div class="img-container"></div>
+    <h2>Update Seller Record</h2>
+    <div class="container">
+        <form action="{{ route('seller.update', $seller->cnic) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-    <form action="{{ route('seller.update', $seller->cnic) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <label for="cnic">CNIC: &nbsp;</label>
-        <input type="text" id="cnic" name="cnic" value="{{ $seller->cnic }}"><br><br>
+            <label for="cnic">CNIC: </label>
+            <div class="form-group">
+                <input type="text" id="cnic" name="cnic" value="{{ $seller->cnic }}">
+            </div>
 
-        <label for="name">Name: &nbsp;</label>
-        <input type="text" id="name" name="name" value="{{ $seller->name }}"><br><br>
+            <label for="name">Name: </label>
+            <div class="form-group">
+                <input type="text" id="name" name="name" value="{{ $seller->name }}">
+            </div>
 
-        <label for="email">Email: &nbsp;</label>
-        <input type="text" id="email" name="email" value="{{ $seller->email }}"><br><br>
+            <label for="email">Email: </label>
+            <div class="form-group">
+                <input type="text" id="email" name="email" value="{{ $seller->email }}">
+            </div>
 
-        <label for="email_verified_at">Email Verified At: &nbsp;</label>
-        <input type="text" id="email_verified_at" name="email_verified_at" value="{{ $seller->email_verified_at }}"><br><br>
+            <label for="email_verified_at">Email Verified At: </label>
+            <div class="form-group">
+                <input type="text" id="email_verified_at" name="email_verified_at" value="{{ $seller->email_verified_at }}">
+            </div>
 
-        <label for="password">Password: &nbsp;</label>
-        <input type="text" id="password" name="password" value="{{ $seller->password }}"><br><br>
+            <label for="password">Password: </label>
+            <div class="form-group">
+                <input type="text" id="password" name="password" value="{{ $seller->password }}">
+            </div>
 
-        <label for="phone_number">Phone No: &nbsp;</label>
-        <input type="text" id="phone_number" name="phone_number" value="{{ $seller->phone_number }}"><br><br>
+            <label for="phone_number">Phone No: </label>
+            <div class="form-group">
+                <input type="text" id="phone_number" name="phone_number" value="{{ $seller->phone_number }}">
+            </div>
 
-        <label for="address">Address: &nbsp;</label>
-        <input type="text" id="address" name="address" value="{{ $seller->address }}"><br><br>
+            <label for="address">Address: </label>
+            <div class="form-group">
+                <input type="text" id="address" name="address" value="{{ $seller->address }}">
+            </div>
 
-        <label for="user_type">User Type: &nbsp;</label>
-        <select id="user_type" name="user_type">
-            <option value="landowner" {{ old('user_type') == 'LandOwner' ? 'selected' : '' }}>Land Owner</option>
-            <option value="patwari" {{ old('user_type') == 'Patwari' ? 'selected' : '' }}>Patwari</option>
-        </select><br><br>
+            <label for="user_type">User Type: </label>
+            <div class="form-group">
+                <select id="user_type" name="user_type">
+                    <option value="landowner" {{ old('user_type') == 'LandOwner' ? 'selected' : '' }}>Land Owner</option>
+                    <option value="patwari" {{ old('user_type') == 'Patwari' ? 'selected' : '' }}>Patwari</option>
+                </select>
+            </div>
 
-
-        <input type="submit" value="Update">
-
-    </form>
-
+            <input type="submit" value="Update">
+        </form>
+    </div>
 </body>
 
 </html>
