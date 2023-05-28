4cc9c7301cb0d9f349956a00d069398f3f2f3b5b
Nehal567 <133264581+Nehal567@users.noreply.github.com>
2023-05-28 21:09:17 +0500

    abc

--- app/Models/Land.php
+++ app/Models/Land.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\GrantUser;
 
 class Land extends Model
 {
@@ -12,4 +13,11 @@ class Land extends Model
     {
         return $this->belongsToMany(Seller::class, 'seller_land');
     }
+    
+    public function grant_user() 
+    {
+        return $this->belongsToMany(GrantUser::class);
+        
+
+        }
 }
